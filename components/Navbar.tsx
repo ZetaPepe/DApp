1550edc8ec4abad5369dfcdea1da61e768ab0a3b
ChristAI <158454707+ZetaPepe@users.noreply.github.com>
2025-04-25 21:56:25 +0800

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,13 @@
-'use client';
-import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+'use client'; 
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'; 
 
-export default function Navbar() {
-  return (
-    <div className="w-full flex justify-end p-4 border-b bg-white shadow-sm">
-      <WalletMultiButton />
+export default function Navbar() { 
+  return ( 
+    <div className="relative w-full h-[calc(100vh-64px)] bg-background"> {/* 使用全局背景颜色变量 */}
+      {/* 绝对定位的按钮 */}
+      <div className="absolute top-4 right-4 z-10">
+        <WalletMultiButton />
+      </div>
     </div>
-  );
+  ); 
 }
