115998ba777900e1a9b5801646d1b4485cf35626
ChristAI <158454707+ZetaPepe@users.noreply.github.com>
2025-04-25 21:19:13 +0800

    Update next-blocks.tsx

--- next-blocks.tsx
+++ next-blocks.tsx
@@ -6,7 +6,6 @@ import { useRef, useState, useEffect } from "react"
 import * as THREE from "three"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGithub, faTelegram, faTwitter } from "@fortawesome/free-brands-svg-icons"
-import WalletConnection from './components/WalletConnection'
 
 const isMobile = () => {
   if (typeof window === 'undefined') return false
@@ -168,8 +167,6 @@ const Scene = () => {
 export default function Component() {
   return (
     <div className="w-full h-screen bg-gray-900 relative">
-      <WalletConnection />
-      {/* 右上角社交媒体图标 */}
       <div className="absolute top-4 left-4 flex space-x-4 z-50">
         <a 
           href="https://github.com/yourusername" 
