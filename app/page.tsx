0a30be6607b2de1622539593ad2982cc8df55321
ChristAI <158454707+ZetaPepe@users.noreply.github.com>
2025-04-25 22:19:47 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
 "use client"
 
-import Navbar from "../components/Navbar"
 import Component from "../next-blocks"
 
 export default function SyntheticV0PageForDeployment() {
-  return (
-    <>
-      <Navbar />  {/* 在页面顶部渲染 Navbar */}
-      <Component />
-    </>
-  );
+  return <Component />
 }
